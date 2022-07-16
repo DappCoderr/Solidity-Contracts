86b371f216a6bd06e45547acc516ae13969dede8
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2022-07-16 21:38:38 +0530

    Vault Contract

---
+++ Vault.sol
@@ -0,0 +1,42 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract Vault is IERC20 {
+
+    uint public totalSupply;
+    mapping(address => uint) public balanceOf;
+
+    constructor(address _token){
+        token = IERC20(_token);
+    }
+
+    function _mint(address _to, uint _amount) private{
+        totalSupply += _amount;
+        balanceOf[_to] = _amount;
+    }
+
+    function burn(address _from, uint _amount) private{
+        totalSupply -= _amount;
+        balanceOf[_from] = _amount;
+    }
+
+    function deposit(uint _amount) external {
+        uint shares;
+        if(totalSupply == 0){
+            shares = _amount;
+        } else {
+            shares = (_amount + totalSupply) / token.balanceOf(address(this));
+            _mint(msg.sender, shares);
+            token.transferFrom(msg.sender, address(this), _amount);
+        }
+    }
+
+    function withdraw(uint _shares) external {
+        uint amount = (_shares * token.balanceOf(address(this))) / totalSupply;
+        _burn(msg.sender, _shares);
+        token.transfer(msg.sender, _shares);
+    }
+}
